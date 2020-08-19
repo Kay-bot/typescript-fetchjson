8c97f36302f04198248482168497908e95f0bfb7
KAY S <KS.KWANRUAN@GMAIL.com>
2020-08-19 15:57:19 +1000

    type annotation

---
+++ features/annotations/variables.ts
@@ -0,0 +1,12 @@
+let apples: number = 5;
+// With let we can reassign apples to a different number
+apples = 10;
+
+let speed: string = 'fast';
+
+let hasName: boolean = true;
+let nothingAtAll: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
