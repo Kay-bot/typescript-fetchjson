14b55c72ec9e31f30b3870f3a14fb94e0ebd1b73
KAY S <KS.KWANRUAN@GMAIL.com>
2020-08-19 21:01:22 +1000

    type arrays

---
+++ features/arrays.ts
@@ -0,0 +1,10 @@
+// Use type annotations when a variable is an empty array
+const carMakers: string[] = [];
+
+// Type inference
+const dates = [new Date(), new Date()];
+
+// Inner arrays
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
+// Type annotation with an empty array of inner arrays
+const carsByMake2: string[][] = [];
