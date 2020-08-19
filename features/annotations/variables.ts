cd8b5dc1749962d906080e4044e7d28f1b20059e
KAY S <KS.KWANRUAN@GMAIL.com>
2020-08-19 16:11:13 +1000

    function annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -26,3 +26,10 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// Important: We don't actually need to add type annotations at all. It will still work just fine.
