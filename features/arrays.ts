a006b6a3ba13279690512a1d2738359941314b3f
KAY S <KS.KWANRUAN@GMAIL.com>
2020-08-20 15:16:54 +1000

    typed arrays

--- features/arrays.ts
+++ features/arrays.ts
@@ -8,3 +8,15 @@ const dates = [new Date(), new Date()];
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
 // Type annotation with an empty array of inner arrays
 const carsByMake2: string[][] = [];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map' - typeScript will provide autocomplete to string method
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
